735c93a2e8f97ffd90bdac55824ad2e93ccef51f
zikai <zikai@zju.edu.cn>
2023-09-14 09:53:28 +0800

    1

--- fedrod_etf.py
+++ fedrod_etf.py
@@ -23,7 +23,7 @@ from util.etf_methods import *
 
 np.set_printoptions(threshold=np.inf)
 
-last_char = "h"
+last_char = "e"
 
 load_switch = True  # True / False
 save_switch = False # True / False
