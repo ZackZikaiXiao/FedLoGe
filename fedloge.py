ade2c5e211aeba6de7412280bb8e42770e84e4cf
ZackZikaiXiao <lewisxzk@outlook.com>
2024-03-08 14:11:43 +0000

    adjust norm

--- fedloge.py
+++ fedloge.py
@@ -177,11 +177,11 @@ if __name__ == '__main__':
 
     # print("STOP")
     # return
-    # torch.manual_seed(args.seed)
-    # torch.cuda.manual_seed(args.seed)
-    # torch.cuda.manual_seed_all(args.seed)
-    # np.random.seed(args.seed)
-    # random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
 
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
